27d5b2add0db3da41bda555368efa04409067e36
fgrreloaded <nk.nitish.nk500@gmail.com>
2026-02-03 21:38:07 +0530

    remove font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
 
-import { Geist, Geist_Mono } from "next/font/google";
-
 import "../index.css";
 import Providers from "@/components/providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "my-better-t-app",
@@ -27,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <Providers>
           <div className="grid grid-rows-[auto_1fr] h-svh">
             {children}
