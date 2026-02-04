6d5ae27f5ef8b5ec77a2769d4c409202fdd9eb79
fgrreloaded <nk.nitish.nk500@gmail.com>
2026-02-04 23:09:06 +0530

    change

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body>
         <Providers>
           <div className="grid grid-rows-[auto_1fr] h-svh">
-            {children}
+            {children} Hello
           </div>
         </Providers>
       </body>
