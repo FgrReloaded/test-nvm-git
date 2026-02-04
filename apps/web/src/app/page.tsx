856742adab478a7368e1456cdde95309769f587d
fgrreloaded <nk.nitish.nk500@gmail.com>
2026-02-04 12:17:54 +0530

    change from upstream

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">Hello</h1>
+      <h1 className="text-4xl font-bold">Hello from upstream</h1>
     </div>
   );
 }
