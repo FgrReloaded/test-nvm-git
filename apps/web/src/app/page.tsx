381bf4966f9df112bc78f6568bd3c029bf47d289
fgrreloaded <nk.nitish.nk500@gmail.com>
2026-02-11 19:16:11 +0530

    update

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">Hello from none</h1>
+      <h1 className="text-4xl font-bold">Hello from up</h1>
     </div>
   );
 }
