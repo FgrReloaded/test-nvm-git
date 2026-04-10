266d09e359432620ed0e32d06eb6b774abf3fce1
Nitish <nk.nitish.nk500@gmail.com>
2026-04-11 00:13:53 +0530

    Update page.tsx

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <h1 className="text-4xl font-bold">Hello from upstream</h1>
-      <h1>New changes from upstream again & again & again</h1>
+      <h1>New changes from upstream again & again & again & again</h1>
     </div>
   );
 }
